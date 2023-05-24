f599328b0e16b6ceaa1674fb0cf997d1b4c3b03d
Nicoogf <nicolasgfalabella@gmail.com>
2023-05-24 09:11:14 -0300

    Prueba de states

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Componente from './components/Componente';
 import Propiedades from './components/Propiedades';
+import Estado from './components/Estado';
 
 function App() {
   return (
@@ -9,48 +10,43 @@ function App() {
 
        
          <header className="App-header">
-         <section>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-                 
-        </section>
+            <section>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+                    
+            </section>
 
-        <section>
-        <Componente msg="Hola soy un componente funcional expresado desde una pro"/>
-        <hr/>
-
-
-        <Propiedades cadena="Esto es una cadena de texto"
-                     numero= {19}
-                     booleano={false}
-                     arreglo={ [1,2,3] }
-                     objeto={{ nombre:"John" , apellido :"Mircha" , correo:"Johnmircha@gmail.com" }}
-                     funcion = { (num) => num * num }
-                     elementoReact= { <i> Esto es un Elemento React</i>}
-                     componenteReact ={<Componente msg="Soy un componente pasado como Prop"/>}
-        /> 
+              <section>
+              <Componente msg="Hola soy un componente funcional expresado desde una pro"/>
+              <hr/>
 
 
+              <Propiedades cadena="Esto es una cadena de texto"
+                          numero= {19}
+                          booleano={false}
+                          arreglo={ [1,2,3] }
+                          objeto={{ nombre:"John" , apellido :"Mircha" , correo:"Johnmircha@gmail.com" }}
+                          funcion = { (num) => num * num }
+                          elementoReact= { <i> Esto es un Elemento React</i>}
+                          componenteReact ={<Componente msg="Soy un componente pasado como Prop"/>}
+              /> 
 
-        <br /> 
-        <br />   
-        <br />   
-        <br />   
-        <br />   
-        <br />
-        <br />   
-        <br />   
-        <br />   
+
+
+        <hr />   
+
+        <Estado />
+
         </section>
 
         </header>
---
+++ src/components/Estado.js
@@ -0,0 +1,28 @@
+import React , {Component} from "react";
+
+export default class Estado extends Component{
+
+
+    constructor(props){
+        super(props);
+        this.state={
+            contador:0,
+        }
+
+        setInterval(() => {
+            this.setState({
+                contador:this.state.contador + 1 
+            })
+        }, 100);
+    }
+
+
+     render(){
+        return(
+            <div> 
+                <h2>El state</h2>
+                <p> {this.state.contador} </p>
+            </div>
+        )
+     }
+}
